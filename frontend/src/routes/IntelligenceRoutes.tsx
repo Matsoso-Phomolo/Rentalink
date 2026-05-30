e78d1e587be3f0787040fea8929ccf60e445258d
Matsoso-Phomolo <phomolomatsoso@gmail.com>
2026-05-30 15:26:05 +0200

    Add intelligence route group

--- frontend/src/routes/IntelligenceRoutes.tsx
+++ frontend/src/routes/IntelligenceRoutes.tsx
@@ -1,1 +1,24 @@
+import { Route } from "react-router-dom";
 
+import IntelligenceLayout from "../layouts/IntelligenceLayout";
+import DistrictIntelligenceDashboard from "../pages/DistrictIntelligenceDashboard";
+import LandlordPortfolioDashboard from "../pages/LandlordPortfolioDashboard";
+import NationalIntelligenceDashboard from "../pages/NationalIntelligenceDashboard";
+import OperationalAlertsDashboard from "../pages/OperationalAlertsDashboard";
+import TenantFinancialDashboard from "../pages/TenantFinancialDashboard";
+
+export function IntelligenceRoutes() {
+  return (
+    <Route path="/intelligence" element={<IntelligenceLayout />}>
+      <Route index element={<NationalIntelligenceDashboard />} />
+      <Route path="national" element={<NationalIntelligenceDashboard />} />
+      <Route
+        path="district/:districtId"
+        element={<DistrictIntelligenceDashboard />}
+      />
+      <Route path="portfolio" element={<LandlordPortfolioDashboard />} />
+      <Route path="tenant/:tenantId" element={<TenantFinancialDashboard />} />
+      <Route path="alerts" element={<OperationalAlertsDashboard />} />
+    </Route>
+  );
+}
